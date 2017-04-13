18781ae500b1eb788c25e58f6a9c51ae324f6b03
レオン <valurate.mac@watarusuda-no-MacBook-Pro.local>
2017-04-13 20:36:10 +0900

    Initial Commit

---
+++ mingo2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mingo2
+//
+//  Created by valurate.mac on 2017/04/13.
+//  Copyright © 2017年 valurate. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ mingo2Tests/mingo2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  mingo2Tests.swift
+//  mingo2Tests
+//
+//  Created by valurate.mac on 2017/04/13.
+//  Copyright © 2017年 valurate. All rights reserved.
+//
+
+import XCTest
+@testable import mingo2
+
+class mingo2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
